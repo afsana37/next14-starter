c68f56eaa7b6428ab06bc3c076fa4b6f7a7f091b
afsana <afsanamist37@gmail.com>
2024-03-21 00:05:27 +0100

    admin and login logout buttons added

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -23,11 +23,23 @@ const Links = () => {
         },
     ];
 
+    //Temporary
+    const session = true
+    const isAdmin = true
+
     return(
        <div className={styles.links}>
         {links.map((link=>(
            <NavLink item={link} key={link.title}/>
         )))}
+          {session ? (
+            <>
+            {isAdmin && <NavLink item={{title:"Admin",path: "/admin"}}/>}
+            <button>Logout</button> 
+            </>
+          ) : (
+            <NavLink item={{title:"Login", path:"/login"}}/>
+          )}
        </div> 
     )
 }
