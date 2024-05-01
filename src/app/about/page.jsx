1a506b784d409265f9892c36eb955dffef2d5a04
afsana <afsanamist37@gmail.com>
2024-05-01 23:19:36 +0200

    external images added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,7 +6,7 @@ const AboutPage = () => {
     return (
         <div>
             <div className={styles.imgContainer}>
-                <Image src="/about.png" alt="" fill/> 
+                <Image src="https://images.pexels.com/photos/18759215/pexels-photo-18759215/free-photo-of-streetlights-in-city.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" fill/> 
             </div>
             
         </div>
