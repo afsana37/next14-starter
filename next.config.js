61dc9adb2bd3b4704ea6528cb51f2ccb805deefd
afsana <afsanamist37@gmail.com>
2024-05-25 15:03:49 +0200

    try to load static images in deployed version

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
+const repo = "next14-starter";
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
 const nextConfig = {
+    trailingSlash: true,
     output: process.env.NODE_ENV !== "production" ? undefined: "export",
+    basePath: basePath,
+    assetPrefix: assetPrefix,
     images:{
       unoptimized: envImageUnoptimize,
       remotePatterns:[
